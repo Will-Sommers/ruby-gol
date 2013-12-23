43de518c104b57b719ff8a7d8defd9a5cfd1930c
Will Sommers <will.sommers@gmail.com>
2013-12-22 21:25:57 -0600

    Cosmetic changeS

--- gol.rb
+++ gol.rb
@@ -183,8 +183,8 @@ class Cell
 
   def find_and_store_neighbors
     possible_neighbors = [[x_coord - 1, y_coord + 1], [x_coord, y_coord + 1], [x_coord + 1, y_coord + 1],
-            [x_coord - 1, y_coord],                             [x_coord + 1, y_coord],
-            [x_coord - 1, y_coord - 1], [x_coord, y_coord - 1], [x_coord + 1, y_coord - 1]]
+                          [x_coord - 1, y_coord],                             [x_coord + 1, y_coord],
+                          [x_coord - 1, y_coord - 1], [x_coord, y_coord - 1], [x_coord + 1, y_coord - 1]]
 
     self.neighbors = possible_neighbors
       .select { |x, y|
